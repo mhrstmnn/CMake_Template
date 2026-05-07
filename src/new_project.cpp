3fc6d0c5d68c05cf0a69d352602b94ae58e11e4b
Michael Horstmann <mhorstmann@gams.com>
2026-05-07 18:34:57 +0200

    Improve code

--- src/new_project.cpp
+++ src/new_project.cpp
@@ -7,8 +7,9 @@ namespace new_project {
 void print_args(const int argc, const char *argv[]) {
   std::cout << "argc: " << argc << "\n\n"
             << "argv:\n";
-  for (unsigned int i{}; i < static_cast<unsigned int>(argc); ++i)
+  for (unsigned int i{}; i < static_cast<unsigned int>(argc); ++i) {
     std::cout << "- " << argv[i] << '\n';
+  }
 }
 
 } // namespace new_project
--- src/new_project.hpp
+++ src/new_project.hpp
@@ -4,4 +4,4 @@ namespace new_project {
 
 void print_args(int argc, const char *argv[]);
 
-}
+} // namespace new_project
